378480b9602ae9c083d4942c797ae3af8bb89d00
HOME <HOME@HOME-PC>
2018-11-25 16:48:10 +0800

    ...

--- src/com/minicms/test/Test.java
+++ src/com/minicms/test/Test.java
@@ -1,11 +1,5 @@
 package com.minicms.test;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import com.minicms.util.DButil;
-
 public class Test {
 	public static void main(String[] args) {
 		System.err.println("//");
